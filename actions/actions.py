87add799d3610a9ad2035d594776cdb3539eaa35
Anish Biswas <anish.biswas@hs-augsburg.de>
2024-04-27 13:14:04 +0200

    Rasa basic code

---
+++ actions/actions.py
@@ -0,0 +1,17 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            text="Sorry, I can not answer this question. Find more information here: https://www.mercedes-benz.de/")
+        return []
