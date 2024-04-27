a88efe2b77adfbeb9813fbfc71fa38321f894935
joaquin-mari <158082709+joaquin-mari@users.noreply.github.com>
2024-04-27 16:52:01 +0200

    Created slot intent

--- actions/actions.py
+++ actions/actions.py
@@ -1,17 +1,38 @@
+# actions.py
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-
-class ActionHelloWorld(Action):
-
+class ActionAskAddress(Action):
     def name(self) -> Text:
-        return "action_default_fallback"
+        return "action_ask_address"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(
-            text="Sorry, I can not answer this question. Find more information here: https://www.mercedes-benz.de/")
+        dispatcher.utter_message("Sure, could you please provide your delivery address?")
+        return []
+
+class ActionAskPizzaType(Action):
+    def name(self) -> Text:
+        return "action_ask_pizza_type"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("What type of pizza would you like to order?")
+        return []
+
+class ActionConfirmOrder(Action):
+    def name(self) -> Text:
+        return "action_confirm_order"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        pizza_type = tracker.get_slot("pizza_type")
+        address = tracker.get_slot("address")
+        print(f"Extracted pizza type: {pizza_type}")  # Debug statement
+        print(f"Extracted address: {address}")  # Debug statement
+        dispatcher.utter_message(f"Sure, I've got your order for {pizza_type} pizza. It will be delivered to {address}.")
         return []
